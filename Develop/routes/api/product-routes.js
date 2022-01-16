fda20212ce993349385bd43446cf4781e75b4d53
thomps0189 <angelathompson2011@gmail.com>
2022-01-16 17:20:20 -0600

    readme

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -6,7 +6,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Category.findAll({
+  Product.findAll({
     include: [{
       model: Category,
       model: Tag
